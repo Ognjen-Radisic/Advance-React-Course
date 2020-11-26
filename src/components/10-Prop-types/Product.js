76800a6ea60b6b5c7463428e2f80dabd4610e0e5
Ognjen Radisic <radisicognjen@gmail.com>
2020-11-26 11:47:15 +0100

    prop types

--- src/components/10-Prop-types/Product.js
+++ src/components/10-Prop-types/Product.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import defaultImage from '../../assets/default-image.jpeg'
 
 const Product = ({ image, name, price }) => {
+  const url = image && image.url
   return (
     <article className='product'>
-      <h4>single product</h4>
-      {/* <img src={image.url} alt={name} />
-      <h4>{name}</h4>
-      <p>{price}</p> */}
+      <img src={url || defaultImage} alt={name || 'default sofa'} />
+      <h4>{name || 'default sofa'}</h4>
+      <p>{price || 3.99}</p>
     </article>
   )
 }
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+}
+
+// we will set default values directly in Product component with short circuit operators
+//
+// Product.defaultProps = {
+//   name: 'sofa',
+//   price: 3.99,
+//   image: defaultImage,
+// }
 
 export default Product
