ba0461b96b983b7194c73f0bd3230a918761a546
Ognjen Radisic <59142427+Ognjen-Radisic@users.noreply.github.com>
2020-12-04 14:36:35 +0100

    react-router useParams

--- src/components/11-React-router/Navbar.js
+++ src/components/11-React-router/Navbar.js
@@ -7,6 +7,12 @@ const Navbar = () => {
         <li>
           <Link to='/'>Home</Link>
         </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+        <li>
+          <Link to='/people'>People</Link>
+        </li>
       </ul>
     </nav>
   )
